1d4eda27a0ada73ddd1346b725215d91b7086bc1
collin <steezmeister@gmail.com>
2013-06-16 14:03:39 -0500

    Finish user edit, pdate, index, and destroy actions

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ resources :sessions, only: [:new, :create, :destroy]
 
 root to: 'static_pages#home'
 
+match '/users', to: 'users#show'
 match "/signup", to: "users#new"
 match "/signin", to: "sessions#new"
 match "/signout", to: 'sessions#destroy', via: :delete
